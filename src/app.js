64445230adcd23570febd0ba6d4d6b3b7385bc0d
Vishal Maurya <mauryavishal9919@gmail.com>
2026-04-09 18:32:30 +0530

    fix db host and port

--- src/app.js
+++ src/app.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const schoolRoutes = require("./routes/schoolRoutes");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/", schoolRoutes);
-
-// module.exports = app;
-
-
 const express = require("express");
 const cors = require("cors");
 
