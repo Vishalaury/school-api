56cab626b007a0964939d4c05044eba6ab0f5dc5
Vishal Maurya <mauryavishal9919@gmail.com>
2026-04-09 16:09:37 +0530

    Removed node_modules

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// const express = require("express");
+// const cors = require("cors");
+// const schoolRoutes = require("./routes/schoolRoutes");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/", schoolRoutes);
+
+// module.exports = app;
+
+
+const express = require("express");
+const cors = require("cors");
+
+const schoolRoutes = require("./routes/schoolRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// test route (optional but good)
+app.get("/", (req, res) => {
+  res.send("API running ");
+});
+
+app.use("/", schoolRoutes);
+
+module.exports = app;
